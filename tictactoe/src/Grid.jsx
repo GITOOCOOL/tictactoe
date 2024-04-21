a62b19df4893c0a2ccfada03ef75482c70334572
Suraj Thakuri <thakurisuraj38@gmail.com>
2024-04-22 00:35:43 +1000

    extracted individual div cell using getElementByID

--- tictactoe/src/Grid.jsx
+++ tictactoe/src/Grid.jsx
@@ -2,43 +2,35 @@ import React from 'react'
 import { useState } from 'react'
 
 const Grid = () => {
-  const [turn, setTurn] = useState('O');
-
-  const [clickedBoxes,setClickedBoxes] = useState([]);
+  
 
   const handleClick = (e) => {
     e.preventDefault(); 
-
-    if(turn === 'O'){
-        setTurn('X')
-    }
-    else if(turn === 'X') {
-        setTurn('O')
-    }
-    // else {
-    //     return;
-    // }
+    console.log((e.target.id))
+    const clickedCell = document.getElementById(e.target.id)
+    clickedCell.classList.add('X')
+    
   };
 
-  console.log(turn);
   return (
     <div className='grid'>
         <div className="row">
-            <div className={`cell ${turn}`} onClick={handleClick} ></div>
-            <div className={`cell ${turn}`} onClick={handleClick} ></div>
-            <div className={`cell ${turn}`} onClick={handleClick} ></div>
+            <div className = "cell" id="1" onClick={handleClick} ></div>
+            <div className={`cell`} id="2" onClick={handleClick} ></div>
+            <div className={`cell`} id="3" onClick={handleClick} ></div>
             
         </div>
         <div className="row">
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
+            <div className = "cell" id="4" onClick={handleClick} ></div>
+            <div className={`cell`} id="5" onClick={handleClick} ></div>
+            <div className={`cell`} id="6" onClick={handleClick} ></div>
         </div>
         <div className="row">
-            <div className="cell"></div>
-            <div className="cell"></div>
-            <div className="cell"></div>
+            <div className = "cell" id="7" onClick={handleClick} ></div>
+            <div className={`cell`} id="8" onClick={handleClick} ></div>
+            <div className={`cell`} id="9" onClick={handleClick} ></div>
         </div>
+
     </div>
   )
 }
